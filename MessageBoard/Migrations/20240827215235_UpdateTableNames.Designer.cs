8d23751b872075a8e29b531e32340db7ce9bcb5c
Samantha Callie <Hoomee90@gmail.com>
2024-08-27 14:53:14 -0700

    add new migration

--- MessageBoard/Migrations/20240827215235_UpdateTableNames.Designer.cs
+++ MessageBoard/Migrations/20240827215235_UpdateTableNames.Designer.cs
@@ -2,6 +2,7 @@
 using MessageBoard.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MessageBoard.Migrations
 {
     [DbContext(typeof(MessageBoardContext))]
-    partial class MessageBoardContextModelSnapshot : ModelSnapshot
+    [Migration("20240827215235_UpdateTableNames")]
+    partial class UpdateTableNames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
