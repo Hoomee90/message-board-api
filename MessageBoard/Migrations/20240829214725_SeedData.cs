f063f8640a538d05c6d44f3ccacbae637081d9fe
Samantha Callie <Hoomee90@gmail.com>
2024-08-29 14:49:14 -0700

    add seed data

---
+++ MessageBoard/Migrations/20240829214725_SeedData.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MessageBoard.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Boards",
+                columns: new[] { "BoardId", "Description", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Write write write write", "Writing" },
+                    { 2, "Art cool stuff", "Art" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Messages",
+                columns: new[] { "MessageId", "BoardId", "Content", "PostedOn", "Username" },
+                values: new object[,]
+                {
+                    { 1, 1, "Woolly Mammoth", new DateTime(2018, 8, 18, 7, 22, 16, 0, DateTimeKind.Unspecified), "Matilda" },
+                    { 3, 7, "Dinosaur", new DateTime(2018, 8, 18, 0, 22, 16, 0, DateTimeKind.Local), "Matilda" },
+                    { 4, 1, "Shark", new DateTime(2022, 8, 28, 11, 22, 16, 0, DateTimeKind.Unspecified), "Pip" },
+                    { 5, 2, "Dinosaur", new DateTime(2010, 12, 1, 1, 23, 50, 0, DateTimeKind.Unspecified), "Bartholomew" },
+                    { 6, 2, "Dinosaur", new DateTime(2018, 8, 18, 0, 22, 16, 0, DateTimeKind.Local), "Rexie" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Boards",
+                keyColumn: "BoardId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Boards",
+                keyColumn: "BoardId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 6);
+        }
+    }
+}
--- MessageBoard/Models/MessageBoardContext.cs
+++ MessageBoard/Models/MessageBoardContext.cs
@@ -9,19 +9,24 @@ public class MessageBoardContext : DbContext
 {
 	public DbSet<Message> Messages { get; set; }
 	public DbSet<Board> Boards { get; set; }
-	public MessageBoardContext (DbContextOptions<MessageBoardContext> options) : base(options)
+	public MessageBoardContext(DbContextOptions<MessageBoardContext> options) : base(options)
 	{
 	}
-	
-	// protected override void OnModelCreating(ModelBuilder builder)
-	// {
-	// 	builder.Entity<Message>()
-	// 		.HasData(
-	// 			new   Message {   MessageId = 1, Username = "Matilda", Content = "Woolly Mammoth", PostedOn = DateTime.Parse("08/18/2018 07:22:16") },
-	// 			new   Message {   MessageId = 2, Username = "Rexie", Content = "Dinosaur", PostedOn = DateTime.Parse("2018-08-18T07:22:16.0000000Z") },
-	// 			new   Message {   MessageId = 3, Username = "Matilda", Content = "Dinosaur", PostedOn = DateTime.Parse("Sat, 18 Aug 2018 07:22:16 GMT") },
-	// 			new   Message {   MessageId = 4, Username = "Pip", Content = "Shark", PostedOn = DateTime.Parse("08/28/2022 11:22:16") },
-	// 			new   Message {   MessageId = 5, Username = "Bartholomew", Content = "Dinosaur", PostedOn = DateTime.Parse("12/1/2010 01:23:50") }
-	// 	);
-	// }
+
+	protected override void OnModelCreating(ModelBuilder builder)
+	{
+		builder.Entity<Message>()
+			.HasData(
+				new Message { MessageId = 1, BoardId = 1, Username = "Matilda", Content = "Woolly Mammoth", PostedOn = DateTime.Parse("08/18/2018 07:22:16") },
+				new Message { MessageId = 6, BoardId = 2, Username = "Rexie", Content = "Dinosaur", PostedOn = DateTime.Parse("2018-08-18T07:22:16.0000000Z") },
+				new Message { MessageId = 3, BoardId = 7, Username = "Matilda", Content = "Dinosaur", PostedOn = DateTime.Parse("Sat, 18 Aug 2018 07:22:16 GMT") },
+				new Message { MessageId = 4, BoardId = 1, Username = "Pip", Content = "Shark", PostedOn = DateTime.Parse("08/28/2022 11:22:16") },
+				new Message { MessageId = 5, BoardId = 2, Username = "Bartholomew", Content = "Dinosaur", PostedOn = DateTime.Parse("12/1/2010 01:23:50") }
+			);
+		builder.Entity<Board>()
+			.HasData(
+				new Board { BoardId = 1, Title = "Writing", Description = "Write write write write" },
+				new Board { BoardId = 2, Title = "Art", Description = "Art cool stuff" }
+			);
+	}
 }
